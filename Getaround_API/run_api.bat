62529eef20ee00caa730e03d257d670894c780cc
Mandy WANG <mtwang35@gmail.com>
2022-07-15 16:39:58 +0200

    initial commit - deployment

--- Getaround_API/run_api.bat
+++ Getaround_API/run_api.bat
@@ -2,9 +2,9 @@ docker run -it^
  -v "C:\Data learning\Jedha\Fullstack\Project\6.Deployment\Getaround_API:/home/app"^
  -p 4030:4030^
  -e PORT=4030^
- -e AWS_ACCESS_KEY_ID="AKIA3DPV47ID2J7XQ2YJ"^
- -e AWS_SECRET_ACCESS_KEY="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"^
- -e BACKEND_STORE_URI="postgresql://bwwjycdkvdeoax:bc3244c33369af38615be00b50bc326d1630cc580739ba3c9408b5da2c3fd133@ec2-54-204-56-171.compute-1.amazonaws.com:5432/dejgcvuprlpsgs"^
+ -e AWS_ACCESS_KEY_ID=""^
+ -e AWS_SECRET_ACCESS_KEY=""^
+ -e BACKEND_STORE_URI=""^
  -e ARTIFACT_ROOT="s3://cours-demo"^
  -e MLFLOW_TRACKING_URI="https://getaround-mlflow-server.herokuapp.com/"^
  getaround-api 
--- Getaround_mlflow/run_mlflow_tracking.bat
+++ Getaround_mlflow/run_mlflow_tracking.bat
@@ -3,8 +3,8 @@ docker run -it^
  -e MLFLOW_TRACKING_URI="https://getaround-mlflow-server.herokuapp.com/"^
  -p 4020:4020^
  -e PORT=4020^
- -e AWS_ACCESS_KEY_ID="AKIA3DPV47ID2J7XQ2YJ"^
- -e AWS_SECRET_ACCESS_KEY="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"^
- -e BACKEND_STORE_URI="postgresql://jpimgxnnsdqmyw:02bc72816b474a9568c4e7a7695a3d64eada3d5c3139f57ca08cc737d7906086@ec2-44-198-82-71.compute-1.amazonaws.com:5432/ddo3qp2kdng60t"^
+ -e AWS_ACCESS_KEY_ID=""^
+ -e AWS_SECRET_ACCESS_KEY=""^
+ -e BACKEND_STORE_URI=""^
  -e ARTIFACT_ROOT="s3://cours-demo"^
  new-mlflow python /home/app/price_prediction.py
